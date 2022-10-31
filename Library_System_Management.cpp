48106ed30758fa26523d4b035b9028d49abf7c1e
alberth ruado <Alberth G. Ruado>
2022-10-31 18:23:08 +0800

    Added Library_System_Management Reference Choice

---
+++ Library_System_Management.cpp
@@ -0,0 +1,18 @@
+/*
+    C++ libraries
+*/
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <fstream>
+using namespace std;
+
+
+
+int main() {
+    int choice; 
+    cout << "LIBRARY SYSTEM MANAGEMENT" << endl;
+    cout << "1) Create Books\n2) Update Books\n3) Update Books\n4) Search Books\n5) Display Books\n6) Exit\n\nEnter choice: ";
+    cin >> choice;
+    return 0;
+}
